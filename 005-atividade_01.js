3b5514dcf569fe60d0e475e9ae0ccbcd890f0a9e
Samuel Baptista <samuelbaptista1998@gmail.com>
2026-08-28 20:37:33 -0300

    atividade 1 cracha de empresa

---
+++ 005-atividade_01.js
@@ -0,0 +1,22 @@
+let entrada = require("prompt-sync")();
+
+let nome = entrada("Qual é o seu nome? ");
+let cargo = entrada("Qual seu cargo ou função? ");
+let dataNasc = entrada("Qual sua data de nascimento? ");
+
+console.log("");
+console.log("******************");
+console.log("CRACHA DA EMPRESA");
+console.log("******************");
+console.log("Nome: " + nome);
+console.log("Variável:" + typeof nome);
+console.log("");
+console.log("Cargo/Função: " + cargo);
+console.log("Variável:" + typeof cargo);
+console.log("");
+console.log("Data de Nascimento: " + dataNasc);
+console.log("Variável:" + typeof dataNasc);
+
+entrada();
+
+//////////////////////////////////////
