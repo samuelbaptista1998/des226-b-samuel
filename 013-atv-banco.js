ec0bcc0120c8bd898ebcade908d95611cbf0f904
Samuel Baptista <samuelbaptista1998@gmail.com>
2026-09-18 20:41:36 -0300

    Create 013-atv-banco.js

---
+++ 013-atv-banco.js
@@ -0,0 +1,49 @@
+let entrada = require("prompt-sync")();
+
+let usuario1 = "samuel";
+let senha1 = "181020";
+let saldo1 = -100;
+
+let usuario2 = "marco";
+let senha2 = "271015";
+let saldo2 = 0;
+
+let usuario3 = "rafael";
+let senha3 = "156075";
+let saldo3 = 500;
+
+let acessoPermitido = false;
+let saldoAtual = 0;
+
+console.log("Insira seus dados para acessar o caixa! ");
+
+let loginUsuario = entrada("Usuário: ").toLowerCase();
+let loginSenha = entrada("Senha: ");
+
+if (loginUsuario === usuario1 && loginSenha === senha1) {
+  acessoPermitido = true;
+  saldoAtual = saldo1;
+} else if (loginUsuario === usuario2 && loginSenha === senha2) {
+  acessoPermitido = true;
+  saldoAtual = saldo2;
+} else if (loginUsuario === usuario3 && loginSenha === senha3) {
+  acessoPermitido = true;
+  saldoAtual = saldo3;
+}
+
+if (acessoPermitido) {
+  console.log("Usuário e senha verificados com sucesso!");
+  console.log("Acesso Permitido!");
+  console.log(`Seu saldo atual é: R$ ${saldoAtual}`);
+  console.log();
+
+  let saque = entrada("Quanto deseja sacar?");
+  if (saque) {
+  }
+
+  //abaixo disso somente se o usuario e senha for errados.
+} else {
+  console.log("Acesso Negado! Usuário ou senha incorretos.");
+}
+
+entrada("Pressione Enter para sair");
