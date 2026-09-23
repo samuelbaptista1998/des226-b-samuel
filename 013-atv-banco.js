0274b0fb595cae2ce1f73a67813a234d9e500598
Samuel Baptista <samuelbaptista1998@gmail.com>
2026-09-23 18:41:10 -0300

    Update 013-atv-banco.js

--- 013-atv-banco.js
+++ 013-atv-banco.js
@@ -1,49 +1,155 @@
 let entrada = require("prompt-sync")();
 
-let usuario1 = "samuel";
-let senha1 = "181020";
-let saldo1 = -100;
+let usuario1 = "klofis";
+let senha1 = "1475369";
+let saldo1 = 500.0;
 
-let usuario2 = "marco";
-let senha2 = "271015";
-let saldo2 = 0;
+let usuario2 = "nilda";
+let senha2 = "1475369";
+let saldo2 = 0.0;
 
-let usuario3 = "jayane";
-let senha3 = "156075";
-let saldo3 = 500;
+let usuario3 = "rean";
+let senha3 = "1475369";
+let saldo3 = -21.0;
 
+let usOk = false;
+let snOk = false;
 let acessoPermitido = false;
-let saldoAtual = 0;
 
-console.log("Insira seus dados para acessar o caixa! ");
+let usuarioLogado = null;
+let saldoUsuarioLogado = null;
 
-let loginUsuario = entrada("Usuário: ").toLowerCase();
+console.log("Insira seus dados para acessar o sistema!");
+
+let loginUsuario = entrada("Nome de usuário: ");
 let loginSenha = entrada("Senha: ");
 
-if (loginUsuario === usuario1 && loginSenha === senha1) {
-  acessoPermitido = true;
-  saldoAtual = saldo1;
-} else if (loginUsuario === usuario2 && loginSenha === senha2) {
-  acessoPermitido = true;
-  saldoAtual = saldo2;
-} else if (loginUsuario === usuario3 && loginSenha === senha3) {
-  acessoPermitido = true;
-  saldoAtual = saldo3;
+// inicio validação usuário - sem operador &
+// if (usuario1 == loginUsuario.toLowerCase()) {
+//     console.log('Nome de usuário veficado com sucesso!');
+//     usOk = true;
+//     usuarioLogado = usuario1;
+//     saldoUsuarioLogado = saldo1;
+// }
+// if (usuario2 == loginUsuario.toLowerCase()) {
+//     console.log('Nome de usuário veficado com sucesso!');
+//     usOk = true;
+//     usuarioLogado = usuario2;
+//     saldoUsuarioLogado = saldo2;
+// }
+// if (usuario3 == loginUsuario.toLowerCase()) {
+//     console.log('Nome de usuário veficado com sucesso!');
+//     usOk = true;
+//     usuarioLogado = usuario3;
+//     saldoUsuarioLogado = saldo3;
+// }
+
+// if (senha1 == loginSenha) {
+//     console.log('Senha veficada com sucesso!');
+//     snOk = true;
+// }
+// if (senha2 == loginSenha) {
+//     console.log('Senha veficada com sucesso!');
+//     snOk = true;
+// }
+// if (senha3 == loginSenha) {
+//     console.log('Senha veficada com sucesso!');
+//     snOk = true;
+// }
+// fim validação de usuário
+
+// início da validação de usuário para evitar repetição quando existirem senhas iguais
+
+if (usuario1 == loginUsuario.toLowerCase() && senha1 == loginSenha) {
+  console.log("Nome de usuário veficado com sucesso!");
+  usOk = true;
+  snOk = true;
+  usuarioLogado = usuario1;
+  saldoUsuarioLogado = saldo1;
+}
+if (usuario2 == loginUsuario.toLowerCase() && senha2 == loginSenha) {
+  console.log("Nome de usuário veficado com sucesso!");
+  usOk = true;
+  snOk = true;
+  usuarioLogado = usuario2;
+  saldoUsuarioLogado = saldo2;
+}
+if (usuario3 == loginUsuario.toLowerCase() && senha3 == loginSenha) {
+  console.log("Nome de usuário veficado com sucesso!");
+  usOk = true;
+  snOk = true;
+  usuarioLogado = usuario3;
+  saldoUsuarioLogado = saldo3;
 }
 
-if (acessoPermitido) {
-  console.log("Usuário e senha verificados com sucesso!");
-  console.log("Acesso Permitido!");
-  console.log(`Seu saldo atual é: R$ ${saldoAtual}`);
-  console.log();
+// fim da validação de usuário
 
-  let saque = entrada("Quanto deseja sacar?");
-  if (saque) {
+if (usOk === true) {
+  if (snOk === true) {
+    acessoPermitido = true;
   }
-
-  //abaixo disso somente se o usuario e senha for errados.
-} else {
-  console.log("Acesso Negado! Usuário ou senha incorretos.");
 }
 
-entrada("Pressione Enter para sair");
+if (!acessoPermitido) {
+  console.log("Acesso negado!");
+} else {
+  console.log(`Acesso permitido para o usuário ${usuarioLogado}`);
+  console.log(`Saldo atual R$ ${saldoUsuarioLogado}`);
+  console.log("");
+  if (saldoUsuarioLogado > 0) {
+    let valorSaque = parseInt(entrada("Digite quanto deseja sacar: R$ "));
+
+    let saldoParcial = saldoUsuarioLogado - valorSaque;
+
+    if (saldoParcial < 0) {
+      console.log(
+        `${usuarioLogado} você tem R$ ${saldoUsuarioLogado} e está tentando sacar R$ ${valorSaque}, seu saldo é insuficiente!`,
+      );
+      console.log("Operação Não efetivada");
+    } else {
+      // tratamento notas - 50 - 20 - 10 - 5 - 2
+      let controleNotas = valorSaque;
+      let calculaNotas = null;
+      let cinquenta;
+      let vinte;
+      let dez;
+      let cinco;
+      let dois;
+
+      cinquenta = parseInt(controleNotas / 50);
+      controleNotas = controleNotas - cinquenta * 50;
+
+      vinte = parseInt(controleNotas / 20);
+      controleNotas = controleNotas - vinte * 20;
+
+      dez = parseInt(controleNotas / 10);
+      controleNotas = controleNotas - dez * 10;
+
+      cinco = parseInt(controleNotas / 5);
+      controleNotas = controleNotas - cinco * 5;
+
+      dois = parseInt(controleNotas / 2);
+      controleNotas = controleNotas - dois * 2;
+
+      if (controleNotas === 1) {
+        console.log(
+          `Não será possível efetuar o saque, pois não temos notas de R$ 1,00.`,
+        );
+      } else {
+        saldoUsuarioLogado = saldoParcial;
+        console.log(`Seu novo saldo é R$ ${saldoUsuarioLogado}`);
+        console.log(`Notas de R$ 50,00: ${cinquenta}`);
+        console.log(`Notas de R$ 20,00: ${vinte}`);
+        console.log(`Notas de R$ 10,00: ${dez}`);
+        console.log(`Notas de R$ 5,00: ${cinco}`);
+        console.log(`Notas de R$ 2,00: ${dois}`);
+
+        console.log("Retire o valor no terminal.");
+      }
+    }
+  } else {
+    console.log("Saldo insuficiente para efetuar um saque!");
+  }
+}
+
+entrada("Pressione enter para finalizar o programa!");
